987dc1d8c9c85ea3c4afef314d420cca37176b40
laniley <melanie.mende@me.com>
2015-01-01 21:31:58 +0100

    save user login to db

---
+++ app/controllers/UserController.php
@@ -0,0 +1,128 @@
+<?php
+
+class UserController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		$fb_id = Input::get('user.fb_id');
+		$name = Input::get('user.name');
+
+		// test the DB-Connection
+		try
+	   {
+	      $pdo = DB::connection('mysql')->getPdo();
+	   }
+	   catch(PDOException $exception)
+	   {
+	      return Response::make('Database error! ' . $exception->getCode() . ' - ' . $exception->getMessage());
+	   }
+
+	   // check if user already exists
+	   $user = DB::table('users')->where('fb_id', $fb_id)->first();
+
+	   $date = new \DateTime;
+
+	   // save login
+	   if($user) // update user
+	   {
+	   	$id = $user->id;
+
+	   	DB::table('users')
+            ->where('id', $id)
+            ->update(array('updated_at' => $date));
+	   }
+	   else // insert user
+	   {
+			$id = DB::table('users')
+				->insertGetId(
+			    	array(
+			    			'fb_id' => $fb_id,
+			    			'name' => $name,
+			    			'created_at' => $date,
+			    			'updated_at' => $date
+			    		)
+					);
+	   }
+
+	   $user = User::findOrFail($id);
+
+	   return '{"user":'.$user.'}';
+	}
+
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+
+}
